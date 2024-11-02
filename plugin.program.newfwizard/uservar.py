95f822136889b2145dc94c8ec14faeb1345b4be0
Newf <ian@kellys.me>
2024-11-02 10:29:34 -0700

    dssdvs

---
+++ plugin.program.newfwizard/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/newf276/wizard/master/builds.json'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/newf276/wizard/master/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
