401e691a2f654aaeeb0a3ae516988443a82c5f02
Newf <ian@kellys.me>
2025-08-02 15:17:12 -0700

    dcdwwd

---
+++ plugin.video.youtube/default.py
@@ -0,0 +1,17 @@
+import sys
+import xbmc, xbmcaddon, xbmcplugin, xbmcgui
+
+try:
+    from urllib import quote  # Python 2
+except ImportError:
+    from urllib.parse import quote  # Python 3
+
+trailer_addon_id = 'newf276.trailers'
+addon_id = xbmcaddon.Addon().getAddonInfo('id')
+
+url = sys.argv[0] + sys.argv[2]
+new_url = 'plugin://newf276.trailers/redirect/?url=' + quote(url)
+xbmc.log("{} - Re-routing {} -> {}".format(addon_id, url, new_url))
+
+li = xbmcgui.ListItem(path=new_url)
+xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
